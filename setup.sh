98b31cbb042e1343df1e0f2238f34d586acaaffa
nabehide <iasoyv@gmail.com>
2025-05-11 21:18:06 +0900

    add: tmux conf

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ ln -sf ~/dotfiles/.vimrc ~/
 mkdir -p ~/.cache/rc
 ln -sf ~/dotfiles/dein.toml ~/.cache/rc/
 ln -sf ~/dotfiles/dein_lazy.toml ~/.cache/rc/
+
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
